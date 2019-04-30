b3d525c4f804d5b8ce9ab58b111cd0ec61eb7d70
MichaelDc86 <lenskymiwa@ya.ru>
2019-04-30 12:55:05 +0300

    11

--- lesson_03/task_9.py
+++ lesson_03/task_9.py
@@ -7,10 +7,14 @@ matrix = [[random.randint(-10, 10) for j in range(0, 4)] for i in range(0, 4)]
 
 def find_max_min_element(tmp_matrix):
     min_element = tmp_matrix[0][0]
-    for i in tmp_matrix:
-        if i[0] <= min_element:
-            min_element = i[0]
-    return min_element
+    max_element = tmp_matrix[0][0]
+    for j in range(0, (len(tmp_matrix[0])+1)):
+        for i in range(0, (len(tmp_matrix)+1)):
+            if tmp_matrix[i][j] <= min_element:
+                min_element = tmp_matrix[i][j]
+        if min_element >= max_element:
+            max_element = min_element
+    return max_element
 
 
 print(matrix, find_max_min_element(matrix))
