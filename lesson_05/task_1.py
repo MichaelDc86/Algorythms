b8b513b2ee1c5e72ac77d0e7fc66e9f5be2f207e
Mike <lenskymiwa@ya.ru>
2019-05-15 23:07:37 +0300

    5 tasks

--- lesson_05/task_1.py
+++ lesson_05/task_1.py
@@ -1,9 +1,8 @@
 # Пользователь вводит данные о количестве предприятий,
 # их наименования и прибыль за 4 квартал (т.е. 4 числа) для каждого предприятия.
 #  Программа должна определить среднюю прибыль (за год для всех предприятий)
-#  и отдельно вывести наименования предприятий, чья прибыль выше среднего и ниже среднего.
+#  и отдельно вывести наименования предприятий, чья прибыль выше среднего и ниже среднего
 
-from collections import Counter
 from collections import defaultdict
 
 
@@ -12,30 +11,33 @@ def inpt():
     firms_dict = defaultdict(list)
 
     for i in range(int(firms_quantity)):
-        firms_income = []
         firm_name = input(f'Введите название предприятия № {i+1}: ')
         for j in range(1, 5):
             income = input(f'Введите прибыль предприятия № {i+1} за {j} квартал: ')
-            firms_income.append(int(income))
-        firms_dict[firm_name] = firms_income
+            firms_dict[firm_name].append(int(income))
     return firms_dict
 
 
-prep_dict = inpt()
-summa = 0
-for k in prep_dict:
-    summa += sum(prep_dict[k])
+def count(prep_dict):
+    middle_val_dict = defaultdict(float)
+    for k in prep_dict:
+        middle_val_tmp = sum(prep_dict[k])/len(prep_dict[k])
+        middle_val_dict[k] = middle_val_tmp
 
-middle_val = summa/len(prep_dict)
-lower_names = []
-higher_names = []
-for k in prep_dict:
-    mid_tmp_val = sum(prep_dict[k])/4
-    if mid_tmp_val > middle_val:
-        higher_names.append(k)
-    else:
-        lower_names.append(k)
+    middle_val = sum(list(middle_val_dict.values()))/len(middle_val_dict)
+    lower_names = []
+    higher_names = []
+    for k in prep_dict:
+        mid_tmp_val = sum(prep_dict[k])/4
+        if mid_tmp_val > middle_val:
+            higher_names.append(k)
+        else:
+            lower_names.append(k)
 
-print(f'{prep_dict}\n{middle_val}\n{lower_names}\n{higher_names}')
+    print(f'Словарь всех фирм: {prep_dict}\n'
+          f'Средняя прибыль за год(4 квартала) всех предприятий: {middle_val}\n'
+          f'Названия предприятий, чья прибыль ниже средней: {lower_names}\n'
+          f'Названия предприятий, чья прибыль выше средней: {higher_names}')
 
-# print(prep_dict)
+
+count(inpt())
