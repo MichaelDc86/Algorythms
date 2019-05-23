b820ff4ff88c6b67f0a9f90fd2a52f1b4490ca96
MichaelDc86 <lenskymiwa@ya.ru>
2019-05-23 15:43:49 +0300

    11

--- lesson_07/task_2.py
+++ lesson_07/task_2.py
@@ -4,30 +4,51 @@
 import random
 
 SIZE = 10
+rez_arr = []
 
 
 def merge(arr):
-    if len(arr) < 2:
-        return arr
 
-    left = merge(arr[:len(arr)//2])
-    right = merge(arr[len(arr)//2:])
+    mid = len(arr) // 2
 
-    rez_arr = []
+    f = arr[:mid]
+    s = arr[mid:]
 
-    while len(left) != 0 or len(right) != 0:
-        if len(left) == 0:
-            rez_arr.append(right[0])
-        if len(right) == 0:
-            rez_arr.append(right[0])
-        if left[0] > right[0]:
-            rez_arr.append(right.pop(0))
-            rez_arr.append(left.pop(0))
-        else:
-            rez_arr.append(left.pop(0))
-            rez_arr.append(right.pop(0))
+    def sort(first, last):
+        if first > last:
+            first, last = last, first
+        return first, last
 
-    return rez_arr
+    if len(f) < 2 and len(s) < 2:
+        tmp_arr = sort(f, s)
+
+        if rez_arr:
+            for i in range(len(tmp_arr)):
+                if len(tmp_arr[i]) == 0:
+                    continue
+                j = 0
+                while j < len(rez_arr) and tmp_arr[i][0] >= rez_arr[j]:
+                    j += 1
+                # rez_arr.insert(j, tmp_arr[i][0])  # очень хотелось сделать insert, так это просто, но скорость)))
+                a = rez_arr[:j]
+                a.append(tmp_arr[i][0])
+                b = rez_arr[j:]
+                rez_arr[:j] = a
+                rez_arr[j+1:] = b
+
+        if len(rez_arr) == 0:
+            if tmp_arr[0]:
+                rez_arr.append(tmp_arr[0][0])
+                rez_arr.append(tmp_arr[1][0])
+            else:
+                rez_arr.append(tmp_arr[1][0])
+
+        return
+
+    merge(f)
+    merge(s)
+
+    return list(rez_arr)
 
 
 array = [round(random.uniform(0, 50), 2) for i in range(SIZE)]
---
+++ lesson_07/task_3.py
@@ -0,0 +1,48 @@
+# Массив размером 2m + 1, где m — натуральное число, заполнен случайным образом.
+# Найдите в массиве медиану. Медианой называется элемент ряда,
+# делящий его на две равные части: в одной находятся элементы,
+# которые не меньше медианы, в другой — не больше медианы.
+#
+# Примечание: задачу можно решить без сортировки исходного массива.
+# Но если это слишком сложно, используйте метод сортировки,
+# который не рассматривался на уроках (сортировка слиянием также недопустима).
+
+import random
+from collections import deque
+
+# n = int(input('Введите натуральное число: '))
+
+# SIZE = 2 * n + 1
+SIZE = 7
+
+# array = [random.randint(-100, 101) for i in range(SIZE)]
+array = [56, -52, 100, -38, -38]
+
+
+def median(arr):
+
+    left, right = [], [SIZE]
+    arr = deque(arr)
+    med = 0
+
+    def median_count(arr_inside, m):
+        d = []
+        u = []
+
+        for i in arr_inside:
+            if i <= m:
+                d.append(i)
+            if i >= m:
+                u.append(i)
+        return d, u
+
+    while len(left) != len(right):
+        med = arr.pop()
+        left, right = median_count(arr, med)
+        arr.appendleft(med)
+
+    return med
+
+
+print(f'Исходный массив: {array}')
+print(f'Медиана: {median(array)}')
